83111c1e80aeb9f203b77dd5767d2bb691ff9bf0
gunner26735 <sidshukla2016@gmail.com>
2023-02-04 15:12:23 +0530

    Added Counters schema for Course ID

--- api/server/model/model_course.js
+++ api/server/model/model_course.js
@@ -1,7 +1,9 @@
 const mongo = require('mongoose')
 const teacherDB = require('./model_teacher')
+const sequencing = require('./model_seq')
 
 var schema = new mongo.Schema({
+    _id :Number,
     walletAddress : {
         type : String,
         ref : teacherDB,
@@ -9,7 +11,7 @@ var schema = new mongo.Schema({
     cName : {
         type : String,
         default : ''
-    },
+    },  
     cDesc : {
         type : String,
         default : ''
@@ -22,8 +24,29 @@ var schema = new mongo.Schema({
         type : String,
         default : ''
     }
+})
 
-}) 
+schema.pre("save", function (next) {
+        let doc = this;
+        sequencing.getSequenceNextValue("_id").
+        then(counter => {
+            console.log("count", counter);
+            if(!counter) {
+                sequencing.insertCounter("_id")
+                .then(counter => {
+                    doc._id = counter;
+                    console.log(doc)
+                    next();
+                })
+                .catch(error => next(error))
+            } else {
+                doc._id = counter;
+                next();
+            }
+        })
+        .catch(error => next(error))
+    });
+
 
 const courseDB  = mongo.model('sl_course',schema)
 
